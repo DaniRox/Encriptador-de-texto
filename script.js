fe5abff135a7f0d3a40862cffcbe0a82a9baddca
Daniela Roxana Escobar <108234679+DaniRox@users.noreply.github.com>
2023-05-21 22:46:48 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -52,11 +52,11 @@ function encriptarMensaje(str){
         boxMsj.style.display = "none";
         msjVacio.style.display = "flex";
       }else {
-        if(str.includes("u")){str = str.replaceAll("u", "ufat")}
-        if(str.includes("o")){str = str.replaceAll("o", "ober")}
-        if(str.includes("a")){str = str.replaceAll("a", "ai")}
-        if(str.includes("i")){str = str.replaceAll("i", "imes")}
         if(str.includes("e")){str = str.replaceAll("e", "enter")}
+        if(str.includes("i")){str = str.replaceAll("i", "imes")}
+        if(str.includes("a")){str = str.replaceAll("a", "ai")}
+        if(str.includes("o")){str = str.replaceAll("o", "ober")}
+        if(str.includes("u")){str = str.replaceAll("u", "ufat")}
         return str;
       }
   }
@@ -86,11 +86,11 @@ function desencriptarMensaje(strEncriptado){
     msjVacio.style.display = "flex";
   }
   for(let i = 0; i < strEncriptado.length; i ++){
-    if(strEncriptado.includes("enter")){strEncriptado = strEncriptado.replaceAll("enter", "e")}
-    if(strEncriptado.includes("imes")){strEncriptado = strEncriptado.replaceAll("imes", "i")}
-    if(strEncriptado.includes("ai")){strEncriptado = strEncriptado.replaceAll("ai", "a")}
-    if(strEncriptado.includes("ober")){strEncriptado = strEncriptado.replaceAll("ober", "o")}
     if(strEncriptado.includes("ufat")){strEncriptado = strEncriptado.replaceAll("ufat", "u")}
+    if(strEncriptado.includes("ober")){strEncriptado = strEncriptado.replaceAll("ober", "o")}
+    if(strEncriptado.includes("ai")){strEncriptado = strEncriptado.replaceAll("ai", "a")}
+    if(strEncriptado.includes("imes")){strEncriptado = strEncriptado.replaceAll("imes", "i")}
+    if(strEncriptado.includes("enter")){strEncriptado = strEncriptado.replaceAll("enter", "e")}
     return strEncriptado;
   }
 }
