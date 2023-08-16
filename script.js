b230de7dea23e67cfe1cba24ba8c18de0d3043c4
DaniRox <daniroxescobar@gmail.com>
2023-08-15 22:27:18 -0300

    reformando diseño

--- script.js
+++ script.js
@@ -1,19 +1,7 @@
-//funcion para hacer crecer el textarea junto con el contenido
-const tx = document.getElementsByTagName("textarea");
-for (let i = 0; i < tx.length; i++) {
-  tx[i].setAttribute("style", "height:" + (tx[i].scrollHeight) + "px;overflow-y:hidden;");
-  tx[i].addEventListener("input", OnInput, false);
-}
-
-function OnInput() {
-  this.style.height = "470px";
-  this.style.height = (this.scrollHeight) + "px";
-}
-
 //variables y funciones para la accion de encriptar y desencriptar
-const msjIn = document.querySelector(".msj-entrada");
+const msgIn = document.querySelector(".msg-in");
 
-const mjsOut = document.querySelector(".msj-salida");
+const msgOut = document.querySelector(".msg-out");
 
 const msjVacio = document.querySelector("#msj-vacio");
 
@@ -21,36 +9,33 @@ const boxMsj = document.querySelector("#msj-salida");
 
 
 function botonEncriptar(){
-  const txtEncriptado = encriptarMensaje(msjIn.value);
-  mjsOut.value = txtEncriptado;
-  boxMsj.style.display = "flex";
-  mjsOut.style.height = "180px";
-  msjVacio.style.display = "none";
-  msjIn.value = "";
+  const txtEncriptado = encriptarMensaje(msgIn.value);
+  msgOut.value = txtEncriptado;
+  msgIn.value = "";
 }
 
 function encriptarMensaje(str){
   let numeros = /[0-9]/g;
   if(str === ""){
     alert("ingrese texto para encriptar");
-    mjsOut.value = txtDesencriptado;
-    boxMsj.style.display = "none";
-    msjVacio.style.display = "flex";
-    msjIn.value = "";
+    msgOut.value = txtDesencriptado;
+    /*boxMsj.style.display = "none";
+    msjVacio.style.display = "flex";*/
+    msgIn.value = "";
   }  
   if(/[0-9]/g.test(str)){
     alert("solo letras minúsculas y sin acentos");
-    msjIn.value = "";
-    mjsOut.value = txtDesencriptado;
-    boxMsj.style.display = "none";
-    msjVacio.style.display = "flex";
+    msgIn.value = "";
+    msgOut.value = txtDesencriptado;
+    /*boxMsj.style.display = "none";
+    msjVacio.style.display = "flex";*/
   }for(let i = 0; i < str.length; i ++){
     if(!/^[a-zA-Z\s]*$/.test(str)){
         alert("solo letras minúsculas y sin acentos");
-        msjIn.value = "";
-        mjsOut.value = txtDesencriptado;
-        boxMsj.style.display = "none";
-        msjVacio.style.display = "flex";
+        msgIn.value = "";
+        msgOut.value = txtDesencriptado;
+        /*boxMsj.style.display = "none";
+        msjVacio.style.display = "flex";*/
       }else {
         if(str.includes("e")){str = str.replaceAll("e", "enter")}
         if(str.includes("i")){str = str.replaceAll("i", "imes")}
@@ -63,27 +48,27 @@ function encriptarMensaje(str){
 }
 
 function botonCopiar() {
-  let contenido = document.querySelector(".msj-salida");
+  let contenido = document.querySelector(".msg-out");
   contenido.select();
   document.execCommand("copy");
 }
 
 function botonDesencriptar(){
-  const txtDesencriptado = desencriptarMensaje(msjIn.value);
-  mjsOut.value = txtDesencriptado;
-  boxMsj.style.display = "flex";
-  mjsOut.style.height = "180px";
-  msjVacio.style.display = "none";
-  msjIn.value = "";
+  const txtDesencriptado = desencriptarMensaje(msgIn.value);
+  msgOut.value = txtDesencriptado;
+  msgOut.style.display = "flex";
+  /*mjsOut.style.height = "180px";
+  msjVacio.style.display = "none";*/
+  msgIn.value = "";
 }
 
 function desencriptarMensaje(strEncriptado){
   if(strEncriptado === ""){
     alert("solo letras minúsculas y sin acentos");
-    msjIn.value = "";
-    mjsOut.value = txtDesencriptado;
-    boxMsj.style.display = "none";
-    msjVacio.style.display = "flex";
+    msgIn.value = "";
+    msgOut.value = txtDesencriptado;
+    /*boxMsj.style.display = "none";
+    msjVacio.style.display = "flex";*/
   }
   for(let i = 0; i < strEncriptado.length; i ++){
     if(strEncriptado.includes("ufat")){strEncriptado = strEncriptado.replaceAll("ufat", "u")}
